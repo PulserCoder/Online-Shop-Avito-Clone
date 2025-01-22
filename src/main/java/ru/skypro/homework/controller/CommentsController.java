e042f2ccc950b54e6c3cd1ea3489e6d97b8c5ad0
daniil <daniltihomirov946@gmail.com>
2025-01-22 12:55:04 +0300

    add comments and file uploader

--- src/main/java/ru/skypro/homework/controller/CommentsController.java
+++ src/main/java/ru/skypro/homework/controller/CommentsController.java
@@ -1,5 +1,10 @@
 package ru.skypro.homework.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -10,47 +15,77 @@ import ru.skypro.homework.dto.comment.Comments;
 import ru.skypro.homework.dto.comment.CreateOrUpdateComment;
 import ru.skypro.homework.service.CommentService;
 
-import java.util.List;
-
 @Slf4j
 @CrossOrigin(value = "http://localhost:3000")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/ads")
+@Tag(name = "Comments", description = "API для управления комментариями")
 public class CommentsController {
 
     private final CommentService commentService;
 
+    @Operation(summary = "Получить все комментарии для объявления", description = "Возвращает список комментариев для объявления по указанному ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Список комментариев успешно получен"),
+            @ApiResponse(responseCode = "404", description = "Объявление с указанным ID не найдено")
+    })
     @GetMapping("{id}/comments")
-    public ResponseEntity<Comments> getComments(@PathVariable int id) {
+    public ResponseEntity<Comments> getComments(
+            @Parameter(description = "ID объявления, для которого нужно получить комментарии", example = "1")
+            @PathVariable int id) {
 
         Comments comments = commentService.getComments(id);
-        return comments != null ? ResponseEntity.ok(comments) : ResponseEntity.notFound().build()   ;
+        return comments != null ? ResponseEntity.ok(comments) : ResponseEntity.notFound().build();
     }
 
-
+    @Operation(summary = "Добавить комментарий к объявлению", description = "Добавляет новый комментарий для указанного объявления.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Комментарий успешно добавлен"),
+            @ApiResponse(responseCode = "404", description = "Объявление с указанным ID не найдено")
+    })
     @PostMapping("{id}/comments")
-    public ResponseEntity<Comment> addComment(@PathVariable int id, @RequestBody CreateOrUpdateComment comment) {
+    public ResponseEntity<Comment> addComment(
+            @Parameter(description = "ID объявления, к которому нужно добавить комментарий", example = "1")
+            @PathVariable int id,
+            @RequestBody CreateOrUpdateComment comment) {
 
-        Comment saveComment = commentService.addComment(comment, 0 ,id);
-
+        Comment saveComment = commentService.addComment(comment, 0, id);
         return saveComment != null ? ResponseEntity.ok(saveComment) : ResponseEntity.notFound().build();
     }
 
+    @Operation(summary = "Удалить комментарий", description = "Удаляет комментарий по его ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Комментарий успешно удалён"),
+            @ApiResponse(responseCode = "403", description = "Нет прав для удаления комментария"),
+            @ApiResponse(responseCode = "404", description = "Комментарий с указанным ID не найден")
+    })
     @PreAuthorize("hasRole('ADMIN') or @commentServiceImpl.isCommentOwner(#commentId)")
     @DeleteMapping("{id}/comments/{commentId}")
-    public void deleteComment(@PathVariable int id, @PathVariable int commentId) {
+    public void deleteComment(
+            @Parameter(description = "ID объявления, к которому относится комментарий", example = "1")
+            @PathVariable int id,
+            @Parameter(description = "ID комментария, который нужно удалить", example = "1")
+            @PathVariable int commentId) {
         commentService.delComment(commentId);
     }
 
+    @Operation(summary = "Обновить комментарий", description = "Обновляет комментарий с указанным ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Комментарий успешно обновлён"),
+            @ApiResponse(responseCode = "403", description = "Нет прав для обновления комментария"),
+            @ApiResponse(responseCode = "404", description = "Комментарий с указанным ID не найден")
+    })
     @PreAuthorize("@commentServiceImpl.isCommentOwner(#commentId)")
     @PatchMapping("{id}/comments/{commentId}")
-    public ResponseEntity<Comment> updateComment(@PathVariable int id,
-                                                 @PathVariable int commentId,
-                                                 @RequestBody CreateOrUpdateComment comment) {
+    public ResponseEntity<Comment> updateComment(
+            @Parameter(description = "ID объявления, к которому относится комментарий", example = "1")
+            @PathVariable int id,
+            @Parameter(description = "ID комментария, который нужно обновить", example = "1")
+            @PathVariable int commentId,
+            @RequestBody CreateOrUpdateComment comment) {
 
-        Comment saveComment = commentService.addComment(comment, commentId ,id);
-
+        Comment saveComment = commentService.addComment(comment, commentId, id);
         return saveComment != null ? ResponseEntity.ok(saveComment) : ResponseEntity.notFound().build();
     }
 }
---
+++ src/main/java/ru/skypro/homework/service/impl/FileUploaderImpl.java
@@ -0,0 +1,94 @@
+package ru.skypro.homework.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import ru.skypro.homework.service.FileUploader;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Objects;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class FileUploaderImpl implements FileUploader {
+
+    @Value("${file.directory}")
+    private String STORAGE_DIRECTORY;
+
+    /**
+     * Сохраняет файл в указанную папку.
+     *
+     * @param file MultipartFile, который нужно сохранить.
+     * @return Путь к сохранённому файлу.
+     * @throws IOException Если файл не удалось сохранить.
+     */
+    @Override
+    public boolean saveFile(MultipartFile file, String fileName) throws IOException {
+        createDirectoryIfNotExists(STORAGE_DIRECTORY);
+
+        fileName = fileName + "." + getExtension(Objects.requireNonNull(file.getOriginalFilename()));
+        Path filePath = Paths.get(STORAGE_DIRECTORY, fileName);
+
+        try (InputStream inputStream = file.getInputStream()) {
+            Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+            log.info("Файл {} успешно сохранён в {}", fileName, filePath.toString());
+        } catch (IOException e) {
+            log.error("Ошибка при сохранении файла {}: {}", fileName, e.getMessage());
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public File getFile(String fileName) throws IOException {
+        Path filePath = Paths.get(STORAGE_DIRECTORY, fileName);
+
+        if (!Files.exists(filePath)) {
+            throw new IOException("Файл " + fileName + " не найден в директории " + STORAGE_DIRECTORY);
+        }
+
+        log.info("Файл {} найден в директории {}", fileName, STORAGE_DIRECTORY);
+        return filePath.toFile();
+    }
+
+    /**
+     * Извлекает расширение файла.
+     *
+     * @param filename Имя файла.
+     * @return Расширение файла.
+     */
+    @Override
+    public String getExtension(String filename) {
+        int index = filename.lastIndexOf(".");
+        if (index == -1) {
+            return "";
+        }
+
+        return filename.substring( index + 1);
+    }
+
+    /**
+     * Создаёт директорию, если она не существует.
+     *
+     * @param directoryPath Путь к директории.
+     * @throws IOException Если не удалось создать директорию.
+     */
+    private void createDirectoryIfNotExists(String directoryPath) throws IOException {
+        Path path = Paths.get(directoryPath);
+        if (!Files.exists(path)) {
+            Files.createDirectories(path);
+            log.info("Директория {} была создана.", directoryPath);
+        }
+    }
+}
+
